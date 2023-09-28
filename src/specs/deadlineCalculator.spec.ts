18ccfe160038b827e4b2ae847d6761df716de1d8
MateIstvanVarga <mateistvanvarga@gmail.com>
2023-09-28 13:04:26 +0200

    feat: Test cases

--- src/specs/deadlineCalculator.spec.ts
+++ src/specs/deadlineCalculator.spec.ts
@@ -1,35 +1,35 @@
 import calculateDeadline from "../deadlineCalculator";
 
 describe("calculateDeadline", () => {
-  describe("Reporting period", () => {
-    test("should return a valid reporting period submit date", () => {
-      const validSubmitDate = new Date("2023-09-27T10:00:00"); // Valid reporting period
+  describe("Within Reporting Period", () => {
+    test("should calculate due date within the same working day", () => {
+      const validSubmitDate = new Date("2023-09-27T10:00:00");
       const result = calculateDeadline(validSubmitDate, 2);
       expect(result).toEqual(new Date("2023-09-27T12:00:00"));
     });
 
-    test("should return error for an invalid reporting period submit date", () => {
-      const invalidSubmitDate = new Date("2023-09-27T18:00:00"); // Outside reporting period
+    test("should throw an error for an invalid reporting period submit date", () => {
+      const invalidSubmitDate = new Date("2023-09-27T18:00:00");
       expect(() => calculateDeadline(invalidSubmitDate, 2)).toThrow(
         "Invalid reporting time!"
       );
     });
   });
 
-  describe("Overlapping", () => {
-    test("should not overlap", () => {
+  describe("Overlapping to Next Periods", () => {
+    test("should not overlap and stay within the same day", () => {
       const validSubmitDate = new Date("2023-09-28T10:00:00");
       const result = calculateDeadline(validSubmitDate, 3);
       expect(result).toEqual(new Date("2023-09-28T13:00:00"));
     });
 
-    test("should overlap to next day", () => {
+    test("should overlap to the next day", () => {
       const validSubmitDate = new Date("2023-09-28T10:00:00");
       const result = calculateDeadline(validSubmitDate, 13);
       expect(result).toEqual(new Date("2023-09-29T15:00:00"));
     });
 
-    test("should overlap to next week", () => {
+    test("should overlap to the next week", () => {
       const validSubmitDate = new Date("2023-09-28T10:00:00");
       const result = calculateDeadline(validSubmitDate, 18);
       expect(result).toEqual(new Date("2023-10-02T12:00:00"));
